e998d083b4cb2493743e5268621abd736845f788
Nsima Peter <peternsima@gmail.com>
2023-10-12 08:19:43 +0000

    add functionalities for the game to Updatescore displayuserchoice and others and also get element by IDs, also commit the if and else if statement and disply massages and also add EventListener

--- js/script.js
+++ js/script.js
@@ -33,3 +33,109 @@ function determineWinner(userChoice, computerChoice) {
     return "Computer wins!";
   }
 }
+
+function updateScore() {
+    const userScoreElement = document.getElementById("user-score");
+    const computerScoreElement = document.getElementById("computer-score");
+    userScoreElement.textContent = userScore;
+    computerScoreElement.textContent = computerScore;
+  }
+  
+  function updateRemainingTries() {
+    const remainingTriesElement = document.getElementById("remaining-tries");
+    remainingTriesElement.textContent = maxTries - tries;
+  }
+  
+  function displayUserChoice(userChoice) {
+    const userChoiceElement = document.getElementById("user-choice");
+    userChoiceElement.textContent = "You chose: " + userChoice;
+  }
+  
+  function displayComputerChoice(computerChoice) {
+    const computerChoiceElement = document.getElementById("computer-choice");
+    computerChoiceElement.textContent = "Computer chose: " + computerChoice;
+  }
+  
+  function setDifficulty(difficulty) {
+    if (gameInProgress) return;
+    if (difficulty === "easy") {
+      maxTries = 7; //
+    } else if (difficulty === "normal") {
+      maxTries = 5;
+      computerDelay = 1000;
+    } else if (difficulty === "hard") {
+      maxTries = 3;
+      computerDelay = 500;
+    }
+    updateRemainingTries();
+  }
+  
+  function congratulateWinner() {
+    let message = "";
+    if (userScore > computerScore) {
+      message = "Congratulations! You have the highest score.";
+    } else if (userScore < computerScore) {
+      message = "Well played! Keep trying to beat the computer.";
+    } else {
+      message = "It's a tie! Try to win the next round.";
+    }
+    const winnerMessageElement = document.getElementById("winner-message");
+    winnerMessageElement.textContent = message;
+  }
+  
+  function playGame(userChoice) {
+    if (tries < maxTries) {
+      const computer = computerChoice();
+      displayComputerChoice(computer);
+      const message = determineWinner(userChoice, computer);
+      const messageElement = document.getElementById("message");
+      messageElement.textContent = message;
+      updateScore();
+      tries++;
+      updateRemainingTries();
+      if (tries === maxTries) {
+        congratulateWinner();
+        setTimeout(() => {
+          alert("Game over! You've reached the maximum number of tries.");
+          resetGame();
+        }, 100);
+      }
+    }
+  }
+  
+  function resetGame() {
+    userScore = 0;
+    computerScore = 0;
+    tries = 0;
+    gameInProgress = false;
+    updateScore();
+    updateRemainingTries();
+    document.getElementById("message").textContent = "Make your choice!";
+    document.getElementById("computer-choice").textContent = "";
+    document.getElementById("winner-message").textContent = "";
+  
+    document.querySelectorAll('input[name="difficulty"]').forEach((radio) => {
+      radio.disabled = false;
+    });
+  }
+  
+  function startGame() {
+    gameInProgress = true;
+    document.querySelectorAll('input[name="difficulty"]').forEach((radio) => {
+      radio.disabled = true;
+    });
+  }
+  
+  document.querySelectorAll(".choices button").forEach((button) => {
+    button.addEventListener("click", function () {
+      const userChoice = this.id;
+      const difficulty = document.querySelector(
+        'input[name="difficulty"]:checked'
+      ).value;
+      setDifficulty(difficulty);
+      startGame();
+      playGame(userChoice);
+    });
+  });
+  
+  window.addEventListener("load", resetGame);
